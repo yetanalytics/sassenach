95ea89150f9bad4af843ae9b915d4283e283343e
Milt Reder <milt@yetanalytics.com>
2018-11-02 10:32:42 -0400

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sassenach
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/yetanalytics/sassenach_test.clj
@@ -0,0 +1,7 @@
+(ns com.yetanalytics.sassenach-test
+  (:require [clojure.test :refer :all]
+            [com.yetanalytics.sassenach :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
