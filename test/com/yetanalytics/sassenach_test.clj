ea65ed137a9d980595f34f1b4a8f4315c921009d
Milt Reder <milt@yetanalytics.com>
2018-11-02 15:17:44 -0400

    a test why not

--- test/com/yetanalytics/sassenach_test.clj
+++ test/com/yetanalytics/sassenach_test.clj
@@ -1,7 +1,30 @@
 (ns com.yetanalytics.sassenach-test
   (:require [clojure.test :refer :all]
-            [com.yetanalytics.sassenach :refer :all]))
+            [com.yetanalytics.sassenach :refer :all]
+            [clojure.java.io :as io]
+            [clojure.string :as cs])
+  (:import [java.io File]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn cleanup-fixture
+  [f]
+  (try (f)
+       (finally
+         (let [outf (io/file "target/test")]
+           (when (.exists outf)
+             (doseq [^File f (reverse (file-seq outf))]
+               (.delete f)))))))
+
+(use-fixtures :each cleanup-fixture)
+
+(deftest compile-test
+  (testing "Compile files from paths"
+    (compile! "test_resources/valid/style.scss"
+              "target/test/style.css"
+              :source-map true)
+    (is (= "h1 {
+  color: red; }
+
+body {
+  margin: 0 0 0 0; }
+/*# sourceMappingURL=style.css.map */"
+           (slurp "target/test/style.css")))))
